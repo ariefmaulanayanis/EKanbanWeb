9cfa6219303df6c8f953316360fe0d9ad7718c04
arief.maulana.yanis <arief.maulana.yanis@gmail.com>
2021-03-05 17:34:41 +0700

    Add project files.

---
+++ EKanbanWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EKanbanWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
