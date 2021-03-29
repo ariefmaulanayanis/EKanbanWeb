f3d1ffa6027911f7b11e2a3d74936c9537c160a6
arief.maulana.yanis <arief.maulana.yanis@gmail.com>
2021-03-29 21:03:58 +0700

    [TrolleyStatus], [MonthlyReport], [DailyReport]

--- EKanbanWeb/Controllers/MonthlyReportController.cs
+++ EKanbanWeb/Controllers/MonthlyReportController.cs
@@ -1,11 +1,15 @@
 ﻿using EKanbanWeb.Data;
+using EKanbanWeb.Helpers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,12 +22,37 @@ namespace EKanbanWeb.Controllers
             base(logger, accessor, configuration, hostEnvironment)
         {
             DbContext = context;
+            FrmHelp = new FormHelper(DbContext);
         }
 
         public IActionResult Index()
         {
             IndexPrep();
+            ViewBag.Month = DateTime.Today.ToString("yyyy-MM");
+            ViewBag.Lines = FrmHelp.GetLineList();
             return View();
+        }
+
+        public string GetData(int lineId, string month)
+        {
+            try
+            {
+                string sp = "sp_MonthlyReportGetData";
+                List<SqlParameter> param = new List<SqlParameter>
+                {
+                    new SqlParameter("@lineId", lineId),
+                    new SqlParameter("@month", month??"")
+                };
+                DataTable dt = SqlHelp.ExecQuery(sp, param);
+
+                string json = JsonConvert.SerializeObject(dt);
+                return json;
+            }
+            catch (Exception e)
+            {
+                LogHelp.WriteErrorLog(e);
+            }
+            return "";
         }
     }
 }
--- EKanbanWeb/Controllers/TrolleyStatusController.cs
+++ EKanbanWeb/Controllers/TrolleyStatusController.cs
@@ -2,11 +2,14 @@
 using EKanbanWeb.Helpers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,32 +17,55 @@ namespace EKanbanWeb.Controllers
 {
     public class TrolleyStatusController : BaseController
     {
-        private MailHelper mailHelper;
+        //private MailHelper mailHelper;
         public TrolleyStatusController(EKanbanWebDBContext context,
             ILogger<DailyReportController> logger, IHttpContextAccessor accessor, IConfiguration configuration, IHostEnvironment hostEnvironment) :
             base(logger, accessor, configuration, hostEnvironment)
         {
             DbContext = context;
-            mailHelper = new MailHelper(logger, hostEnvironment);
+            FrmHelp = new FormHelper(DbContext);
+            //mailHelper = new MailHelper(logger, hostEnvironment);
         }
 
         public IActionResult Index()
         {
             IndexPrep();
+            ViewBag.Lines = FrmHelp.GetLineList();
             return View();
         }
 
-        public IActionResult SendMail()
+        public string GetData(int lineId)
         {
-            string subject="Test Send Mail";
-            string to="pepenk.en.ciel@gmail.com";
-            string body="Ini hanya email test";
-            if(!mailHelper.SendMail(subject, to, body))
+            try
             {
-                ModelState.AddModelError(string.Empty, mailHelper.errMessage);
+                string sp = "sp_KanbanStatusGetData";
+                List<SqlParameter> param = new List<SqlParameter>
+                {
+                    new SqlParameter("@lineId", lineId)
+                };
+                DataTable dt = SqlHelp.ExecQuery(sp, param);
+
+                string json = JsonConvert.SerializeObject(dt);
+                return json;
             }
-            IndexPrep();
-            return View("Index");
+            catch (Exception e)
+            {
+                LogHelp.WriteErrorLog(e);
+            }
+            return "";
         }
+
+        //public IActionResult SendMail()
+        //{
+        //    string subject="Test Send Mail";
+        //    string to="pepenk.en.ciel@gmail.com";
+        //    string body="Ini hanya email test";
+        //    if(!mailHelper.SendMail(subject, to, body))
+        //    {
+        //        ModelState.AddModelError(string.Empty, mailHelper.errMessage);
+        //    }
+        //    IndexPrep();
+        //    return View("Index");
+        //}
     }
 }
