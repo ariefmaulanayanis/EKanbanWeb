b1529945865c7bdf56aa811618cf6d9db8d73b14
arief.maulana.yanis <arief.maulana.yanis@gmail.com>
2021-04-12 15:56:48 +0700

    [Dashboard] on progress

---
+++ EKanbanWeb/Controllers/DashboardController.cs
@@ -0,0 +1,29 @@
+﻿using EKanbanWeb.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EKanbanWeb.Controllers
+{
+    public class DashboardController : BaseController
+    {
+        public DashboardController(EKanbanWebDBContext context,
+            ILogger<DashboardController> logger, IHttpContextAccessor accessor, IConfiguration configuration, IHostEnvironment hostEnvironment) :
+            base(logger, accessor, configuration, hostEnvironment)
+        {
+            DbContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            IndexPrep();
+            return View();
+        }
+    }
+}
